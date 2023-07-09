e71a056ba327dbb1ea5becdf81f5c77c07812c58
Jihyun Bang <01066303821jh@gmail.com>
2023-07-10 01:39:50 +0900

    불필요한 파일 제거

--- src/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import App from "./App.js";
-// App component => html
-const $app = document.querySelector('#app');
-new App({
-  $target : $app,
-  initialState : "처음 받아온 data(fetch로 받아오기)"
-})
--- src/pages/HomePage.js
+++
@@ -1,6 +0,0 @@
-export default function HomePage({ $target, initialState }) {
-  // 태그를 만든다. 
-  const $div = document.createElement("div"); 
-  $target.appendChild($div);
-
-}
