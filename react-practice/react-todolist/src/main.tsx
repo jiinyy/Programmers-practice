534e985f9f735354bd544efbd269237fbae3d28b
Jihyun Bang <01066303821jh@gmail.com>
2023-08-17 03:37:22 +0900

    App7.tsx 연습완료

--- react-practice/react-todolist/src/main.tsx
+++ react-practice/react-todolist/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App6 from './App6.tsx'
+import App7 from './App7.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App6 />
+    <App7 />
   </React.StrictMode>,
 )
